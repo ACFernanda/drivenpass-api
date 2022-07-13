1453817b501cc99927178ed9816bdd402a79654d
acfernanda <acfernanda98@gmail.com>
2022-07-13 11:56:48 -0300

    Add routes file

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import express, { json } from "express";
 import "express-async-errors";
 
 import "./config/setup.js";
+import router from "./routes/index.js";
 
 const app = express();
 app.use(json());
-// app.use(router);
+app.use(router);
 // app.use(handleErrorsMiddleware);
 
 const port = +process.env.PORT || 5000;
