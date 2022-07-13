cb1c2094a458c89770b333dfec5fabb44c7907ee
acfernanda <acfernanda98@gmail.com>
2022-07-13 11:45:55 -0300

    Initialize project

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import pg from "pg";
+import "./setup.js";
+
+const { Pool } = pg;
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: null,
+};
+
+if (process.env.MODE === "PROD") {
+  configDatabase.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const db = new Pool(configDatabase);
+export default db;
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+import "express-async-errors";
+
+import "./config/setup.js";
+
+const app = express();
+app.use(json());
+// app.use(router);
+// app.use(handleErrorsMiddleware);
+
+const port = +process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+});
+
+export default app;
