6f04158f00f2660774137b52f852f191a7c97cd1
ElderGr <gbielrs@gmail.com>
2022-07-28 14:42:32 -0300

    Preparando revisão de código

--- src/schemas/wifiSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-import { CreateWifiData } from "../repositories/wifiRepository.js";
-
-export const wifiSchema = joi.object<CreateWifiData>({
-  title: joi.string().required(),
-  name: joi.string().required(),
-  password: joi.string().required(),
-});
